a28ae4a407c0323019621293701738609e9e412d
ArtificeJojo <67291255+ArtificeJojo@users.noreply.github.com>
2026-07-26 18:22:03 -0400

    Download MP3
    Basic function added, Can paste and download audio from youtube

---
+++ ytmp3.py
@@ -0,0 +1,14 @@
+﻿import yt_dlp
+
+def DownloadAudio(url):
+    ydl_opts = {'format': 'bestaudio/best','postprocessors':[{'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3', 'preferredquality':'192'}],
+                }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def main():
+    url = "https://youtu.be/QmbIrBrCozA"
+    DownloadAudio(url)
+
+main()
